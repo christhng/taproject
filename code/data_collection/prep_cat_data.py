ed45b6e0c33560c6dd0b016d46ad7cb7ee7db9c6
Jun Quan Tham <junquantham@Juns-MacBook-Pro.local>
2017-02-13 19:27:39 +0800

    updated prep_cat

--- code/data_collection/prep_cat_data.py
+++ code/data_collection/prep_cat_data.py
@@ -11,6 +11,13 @@ results = c.fetchall()
 
 records = []
 
+# clear the table
+del_cat_sql = "delete from stg_categories;"
+c.execute(del_cat_sql)
+
+conn.commit()
+
+# iterate through each record and create biz_id - category as a record
 for result in results:
     biz_id = result[0]
     cats = result[1]
@@ -20,6 +27,7 @@ for result in results:
         record = [biz_id,cat]
         records.append(record)
 
+# insert into stg_categories table
 c.executemany('INSERT INTO stg_categories VALUES (?,?)', records)
 
 conn.commit()
